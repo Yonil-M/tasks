3cdeec1e228b67c905850b9f93a7b30efb569a3a
Artyon2033 <gonzalesromanhiroshiyaisen@gmail.com>
2022-12-06 17:56:14 -0500

    video 10

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:tasks/pages/home_page.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Tasks Firebase",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+ CollectionReference tasksReference= FirebaseFirestore.instance.collection('tasks');
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+   
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Tasks"),),
+
+      body: StreamBuilder(
+        stream: tasksReference.snapshots(),
+        builder:(BuildContext context,AsyncSnapshot snap) {
+          if(snap.hasData){
+            QuerySnapshot collection=snap.data;
+            List<QueryDocumentSnapshot> docs=collection.docs;
+            List<Map<String,dynamic>> docsMap=docs.map((e)=>e.data()as Map<String,dynamic>).toList();
+            print(docsMap);
+            return ListView.builder(
+              itemCount: docsMap.length,
+              itemBuilder: (BuildContext context,int  index) {
+                return ListTile(
+                  title: Text(docsMap[index]["title"]),
+                );
+                
+              },
+            );
+          }
+
+          return Center(child: CircularProgressIndicator(),);
+          
+        }, 
+      ),
+
+    );
+  }
+}
